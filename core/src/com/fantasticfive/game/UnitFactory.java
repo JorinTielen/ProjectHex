59b05c20fe2f4787eadb7f46962bf43c54c82f24
Maxim Doolaard <363112@student.fontys.nl>
2017-10-09 10:48:50 +0200

    Started implementing Units

--- core/src/com/fantasticfive/game/UnitFactory.java
+++ core/src/com/fantasticfive/game/UnitFactory.java
@@ -10,18 +10,41 @@ public class UnitFactory {
     private List<Unit> unitPresets;
 
     public UnitFactory() {
-
+        fillList();
     }
 
     private List<Unit> getUnitPresets() {
         return Collections.unmodifiableList(unitPresets);
     }
 
-    private Unit createUnit(UnitType unitType, Point location) {
-        throw new NotImplementedException();
+    private Unit createUnit(UnitType unitType, Point location, Player owner) {
+        Unit unit = null;
+        switch (unitType) {
+            case SWORDSMAN:
+                unit = getUnitPreset(UnitType.SWORDSMAN);
+                break;
+            case ARCHER:
+                unit = getUnitPreset(UnitType.ARCHER);
+                break;
+            case SCOUT:
+                unit = getUnitPreset(UnitType.SCOUT);
+                break;
+        }
+        unit.setOwner(owner);
+        unit.move(location);
+        return unit;
     }
 
     private Unit getUnitPreset(UnitType unitType) {
-        throw new NotImplementedException();
+        for(Unit unit : getUnitPresets()) {
+            if(unit.getType() == unitType) {
+                return unit;
+            }
+        }
+        return null;
+    }
+
+    private void fillList() {
+        unitPresets.add(new Unit(UnitType.SWORDSMAN, 100, 15, 30, 1, 2, 100, 1, false, 150, null));
     }
 }
