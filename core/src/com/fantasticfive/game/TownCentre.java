2e8df5d210f43b66413b07ad1d5662e60c41a107
Tim Verstijnen <tverstijnen@gmail.com>
2017-10-09 11:00:25 +0200

    Implemented all methods for BuildingFactory and Buildings

--- core/src/com/fantasticfive/game/TownCentre.java
+++ core/src/com/fantasticfive/game/TownCentre.java
@@ -1,14 +1,15 @@
 package com.fantasticfive.game;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.fantasticfive.game.enums.BuildingType;
 import com.fantasticfive.game.enums.GroundType;
 
 import java.io.File;
 
 public class TownCentre extends Building {
-    public TownCentre(BuildingType buildingType, int health, Point location,
-                      File image, int purchaseCost, GroundType[] buildableOn,
+    public TownCentre(int health, Point location,
+                      Texture image, GroundType[] buildableOn,
                       Player owner) {
-        super(buildingType, health, location, image, purchaseCost, buildableOn, owner);
+        super(health, location, image, buildableOn, owner);
     }
 }
